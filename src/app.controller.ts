9895d8a06138b6e7dd5cedd473143577c2dca997
Khitrov Aleksandr <khitrov.av@yandex.ru>
2024-10-08 22:39:32 +0300

    timeouts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<any> {
+    await new Promise(f => setTimeout(f, 10000));
+    return this.appService.getHello();
+  }
+
+  @Get('/about')
+  getAbout(): string {
     return this.appService.getHello();
   }
 }
